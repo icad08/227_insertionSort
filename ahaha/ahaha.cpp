4aca44ba5422f2b5238463a4ba98be3e59705ec9
icad08 <irsyadrm08@gmail.com>
2024-03-14 15:39:23 +0700

    commit2

--- ahaha/ahaha.cpp
+++ ahaha/ahaha.cpp
@@ -15,3 +15,11 @@ void input() {
             cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
         }
     }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
