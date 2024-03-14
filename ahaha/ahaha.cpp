bafe47dbdb477ada15e302baca15668a0c123cd5
icad08 <irsyadrm08@gmail.com>
2024-03-14 15:39:56 +0700

    commit3

--- ahaha/ahaha.cpp
+++ ahaha/ahaha.cpp
@@ -23,3 +23,16 @@ void input() {
         cout << "data ke-" << (i + 1) << ": ";
         cin >> arr[i];
     }
+    void InsrtionSort() {
+        for (i = 1; i <= n - 1; i++) { //step 1
+            temp = arr[i]; //step 2
+
+            j = i - i; // step 3
+
+            while (j >= 0 && arr[j] > temp) //step 4 
+            {
+                arr[j + 1] = arr[j]; //step 4a
+                j--; //step 4b
+            }
+            arr[j + 1] = temp;//step 5
+        }
