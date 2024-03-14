b61310e3e738cbb205484ca74ac1ce876cb15faa
icad08 <irsyadrm08@gmail.com>
2024-03-14 15:40:25 +0700

    commit4

--- ahaha/ahaha.cpp
+++ ahaha/ahaha.cpp
@@ -36,3 +36,15 @@ void input() {
             }
             arr[j + 1] = temp;//step 5
         }
+    }void display() {
+        cout << endl;
+        cout << "=================================" << endl;
+        cout << "Element Array yang telah tersusun" << endl;
+        cout << "=================================" << endl;
+        for (int j = 0; j < n; j++) {
+            cout << arr[j] << endl; //output setiap element array pada garis baru
+        }
+        cout << "jumlah pass = " << n - 1 << endl;
+        cout << endl;
+    }
+
